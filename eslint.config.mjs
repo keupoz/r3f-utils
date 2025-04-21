511f0a9a21135ad30079a4455f8865b9e3c1b031
Ivan Sokolov <keupoz@gmail.com>
2025-04-22 00:14:51 +0700

    Update ESLint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,5 +10,16 @@ export default antfu({
 
     'curly': ['error', 'multi-line', 'consistent'],
     'style/brace-style': ['error', '1tbs'],
+
+    'style/jsx-one-expression-per-line': ['error', { allow: 'non-jsx' }],
+    'react-refresh/only-export-components': ['off'],
+
+    'node/prefer-global/buffer': ['error', 'always'],
+    'node/prefer-global/console': ['error', 'always'],
+    'node/prefer-global/process': ['error', 'always'],
+    'node/prefer-global/text-decoder': ['error', 'always'],
+    'node/prefer-global/text-encoder': ['error', 'always'],
+    'node/prefer-global/url-search-params': ['error', 'always'],
+    'node/prefer-global/url': ['error', 'always'],
   },
 })
