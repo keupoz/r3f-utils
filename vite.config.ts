d758086566245a9c604be50e9b84780872b97482
Ivan Sokolov <keupoz@gmail.com>
2025-04-22 01:03:30 +0700

    Use automatic tsconfig paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,9 @@
-import { resolve } from 'node:path'
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '~': resolve(__dirname, './src'),
-    },
-  },
+  plugins: [tsconfigPaths(), react()],
 })
